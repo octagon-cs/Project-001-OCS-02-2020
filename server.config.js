b5fc063c0cdf9add1efa1bcee0f9c62907d61663
ocph23@gmail.com <ocph23@gmail.com>
2020-03-03 23:47:59 +0900

    fix server config

--- server.config.js
+++ server.config.js
@@ -1,13 +1,16 @@
 module.exports = {
     apps: [{
-        name: 'won app',
+        name: 'Won',
         script: 'index.js',
-        instances: 0,
-        exec_mode: 'cluster',
-        watch: true,
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
         env: {
+            NODE_ENV: 'development',
+        },
+        env_production: {
             NODE_ENV: 'production',
-            PORT: '3000'
-        }
-    }]
+        },
+    }, ],
 };
