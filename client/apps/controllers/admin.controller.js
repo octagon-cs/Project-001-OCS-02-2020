b79d458a44fbc9608fea5991e3e9713218d6e918
kristt26 <kristt26@gmail.com>
2020-03-04 02:53:16 +0900

    New Commit

--- client/apps/controllers/admin.controller.js
+++ client/apps/controllers/admin.controller.js
@@ -1,3 +1,5 @@
+import { response } from "express";
+
 angular.module('admin.controller',[])
 .controller('adminHomeController', adminHomeController)
 .controller('admindatakepaladesaController', admindatakepaladesaController)
@@ -117,8 +119,20 @@ function admindatakepaladesaController() {
 function admindataumumdesaController() {
     
 }
-function adminsuratpengantarkkController() {
-    
+function adminsuratpengantarkkController($http, $scope, helperServices, AuthService) {
+    $scope.JenisPermohonan = [];
+    $scope.KepemilikanKTP = helperServices.StatusKepemilikanKTP;
+    // $scope.Init= function(){
+    //     $http({
+    //         method: 'get',
+    //         url: helperServices.url + "/api/jenispermohonan",
+    //         Header: AuthService.getHeader()
+    //     }).then(response=>{
+
+    //     },error=>{
+
+    //     })
+    // }
 }
 function adminsuratpengantarktpController(){
 
