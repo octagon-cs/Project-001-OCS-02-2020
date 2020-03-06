1aae19b0c41808d1dcf25a07aad4b23c257a10c7
masidns <masidns@gmail.com>
2020-03-06 00:37:32 -0800

    n

--- client/apps/routes/admin.routes.js
+++ client/apps/routes/admin.routes.js
@@ -124,11 +124,11 @@ angular.module('admin.router', ['ui.router']).config(function ($stateProvider,$u
             parent: 'admin',
 			controller: 'adminsuratketlainnyaController',
 			templateUrl: '../client/apps/views/admin/suratketlainnya.html'
-		}).state('admin-suratketmenikah', {
-            url: '/suratketmenikah',
+		}).state('admin-suratketnikah', {
+            url: '/suratketnikah',
             parent: 'admin',
-			controller: 'adminsuratketmenikahController',
-			templateUrl: '../client/apps/views/admin/suratketmenikah.html'
+			controller: 'adminsuratketnikahController',
+			templateUrl: '../client/apps/views/admin/suratketnikah.html'
 		});
 		
 });
