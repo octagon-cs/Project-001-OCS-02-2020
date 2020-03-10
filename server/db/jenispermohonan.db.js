fafc37a1c139c28b2af99374578eb674b6485fc3
kristt26 <kristt26@gmail.com>
2020-03-10 18:29:25 +0900

    New

--- server/db/jenispermohonan.db.js
+++ server/db/jenispermohonan.db.js
@@ -46,12 +46,37 @@ jenispermohonan.getById = async (Id) => {
     });
 };
 
+jenispermohonan.getByJenis = async (Jenis) => {
+    return new Promise((resolve, reject) => {
+        pool.query(
+            `SELECT *
+		  FROM
+			jenispermohonan where jenis=? `,
+            [Jenis],
+            (err, result) => {
+                if (err) {
+                    return reject(err);
+                } else {
+                    if (result.length <= 0)
+                        resolve(result)
+                    else {
+                        var item = result[0];
+                        item.persyaratan = JSON.parse(item.persyaratan);
+                        resolve(item);
+                    }
+
+                }
+            }
+        );
+    });
+};
+
 jenispermohonan.post = async (data) => {
     return new Promise((resolve, reject) => {
         try {
             pool.query(
                 'insert into jenispermohonan  (nama, persyaratan, jenis,deskripsi) values(?,?,?,?)',
-                [data.nama, JSON.stringify(data.persyaratan), data.deskripsi],
+                [data.nama, JSON.stringify(data.persyaratan), data.jenis, data.deskripsi],
                 (err, result) => {
                     if (err) {
                         return reject(err);
