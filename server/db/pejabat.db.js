a62447755ca727838061fdbeff5ced39abb95c83
ocph23@gmail.com <ocph23@gmail.com>
2020-03-19 23:22:28 +0900

    change Pejabat

--- server/db/pejabat.db.js
+++ server/db/pejabat.db.js
@@ -4,205 +4,226 @@ const helper = require('../helper');
 const pejabat = {};
 
 pejabat.get = async () => {
-    return new Promise((resolve, reject) => {
-        pool.query(
-            `SELECT
+	return new Promise((resolve, reject) => {
+		pool.query(
+			`SELECT
             pejabat.*,
             jabatan.nama AS namajabatan,
             jabatan.deskripsi
           FROM
             pejabat
             LEFT JOIN jabatan ON jabatan.idjabatan = pejabat.idjabatan`,
-            (error, result) => {
-                if (error) {
-                    return reject(error);
-                } else {
-                    result.forEach(element => {
-                        element.data = JSON.parse(element.data)
-                    });
-                    resolve(result);
-                }
-            }
-        );
-    });
+			(error, result) => {
+				if (error) {
+					return reject(error);
+				} else {
+					result.forEach((element) => {
+						element.data = JSON.parse(element.data);
+					});
+					resolve(result);
+				}
+			}
+		);
+	});
 };
 
 pejabat.getById = async (Id) => {
-    return new Promise((resolve, reject) => {
-        pool.query(
-            `SELECT
+	return new Promise((resolve, reject) => {
+		pool.query(
+			`SELECT
             pejabat.*,
             jabatan.nama AS namajabatan,
             jabatan.deskripsi
           FROM
             pejabat
             LEFT JOIN jabatan ON jabatan.idjabatan = pejabat.idjabatans where idpejabat=? `,
-            [Id],
-            (error, result) => {
-                if (error) {
-                    return reject(error);
-                } else {
-                    if (result.length <= 0)
-                        resolve(result)
-                    else {
-                        var item = result[0];
-                        item.data = JSON.parse(item.data);
-                        resolve(item);
-                    }
-                }
-            }
-        );
-    });
+			[ Id ],
+			(error, result) => {
+				if (error) {
+					return reject(error);
+				} else {
+					if (result.length <= 0) resolve(result);
+					else {
+						var item = result[0];
+						item.data = JSON.parse(item.data);
+						resolve(item);
+					}
+				}
+			}
+		);
+	});
 };
 
 pejabat.post = async (data, hostname) => {
-    return new Promise((resolve, reject) => {
-        try {
-            pool.getConnection((error, connection) => {
-                if (error) {
-                    return reject(error);
-                } else {
-                    if (!data.role) {
-                        connection.query(
-                            'insert into pejabat  (nama, data, foto, idjabatan) values(?,?,?,?)',
-                            [data.nama, JSON.stringify(data.data), data.foto, data.idjabatan],
-                            (error, result) => {
-                                if (error) {
-                                    connection.rollback(function () {
-                                        connection.release();
-                                        return reject(error);
-                                    });
-                                } else {
-                                    data.idpejabat = result.insertId;
-                                    connection.commit(function (err) {
-                                        if (err) {
-                                            return connection.rollback(function () {
-                                                return reject(err);
-                                            });
-                                        }
-                                        return resolve(data);
-                                    });
-                                }
-                            }
-                        );
-                    } else {
-                        var password = bcrypt.hashSync("admin", 8);
-                        connection.query(
-                            'insert into users (username,password,email) values(?,?,?)',
-                            [data.data.email, password, data.data.email],
-                            (error, result) => {
-                                if (error) {
-                                    connection.rollback(function () {
-                                        connection.release();
-                                        return reject(error);
-                                    });
-                                } else {
-                                    data.idusers = result.insertId;
+	return new Promise((resolve, reject) => {
+		try {
+			pool.getConnection((error, connection) => {
+				if (error) {
+					return reject(error);
+				} else {
+					if (!data.role) {
+						connection.query(
+							'insert into pejabat  (nama, nip, data, foto, idjabatan) values(?,?,?,?,?)',
+							[ data.nama, data.nip, JSON.stringify(data.data), data.foto, data.idjabatan ],
+							(error, result) => {
+								if (error) {
+									connection.rollback(function() {
+										connection.release();
+										return reject(error);
+									});
+								} else {
+									data.idpejabat = result.insertId;
+									connection.commit(function(err) {
+										if (err) {
+											return connection.rollback(function() {
+												return reject(err);
+											});
+										}
+										return resolve(data);
+									});
+								}
+							}
+						);
+					} else {
+						var password = bcrypt.hashSync('admin', 8);
+						connection.query(
+							'insert into users (username,password,email) values(?,?,?)',
+							[ data.data.email, password, data.data.email ],
+							(error, result) => {
+								if (error) {
+									connection.rollback(function() {
+										connection.release();
+										return reject(error);
+									});
+								} else {
+									data.idusers = result.insertId;
 
-                                    connection.query(
-                                        'select * from roles where name=?',
-                                        [data.role],
-                                        (err, roleResult) => {
-                                            if (err) {
-                                                connection.rollback(function () {
-                                                    connection.release();
-                                                    return reject(err);
-                                                });
-                                            } else {
-                                                var role = roleResult[0];
-                                                connection.query(
-                                                    'insert into userinrole(idusers,idroles) values(?,?)',
-                                                    [data.idusers, role.idroles],
-                                                    (err, result) => {
-                                                        if (err) {
-                                                            connection.rollback(function () {
-                                                                connection.release();
-                                                                return reject(err);
-                                                            });
-                                                        } else {
-                                                            connection.query(
-                                                                'insert into pejabat  (nama, data, foto, idjabatan, idusers) values(?,?,?,?,?)',
-                                                                [data.nama, JSON.stringify(data.data), data.foto, data.idjabatan, data.idusers],
-                                                                (error, result) => {
-                                                                    if (error) {
-                                                                        return reject(error);
-                                                                    } else {
-                                                                        data.idpejabat = result.insertId;
-                                                                        helper.sendEmailConfirmEmail({
-                                                                            idusers: data.idusers,
-                                                                            email: data.data.email
-                                                                        }, hostname).then(x => {
-                                                                            connection.commit(function (err) {
-                                                                                if (err) {
-                                                                                    return connection.rollback(function () {
-                                                                                        connection.release();
-                                                                                        return reject(err);
-                                                                                    });
-                                                                                } else {
-                                                                                    connection.release();
-                                                                                    return resolve(data);
-                                                                                }
-
-                                                                            });
-                                                                        }, err => {
-                                                                            connection.rollback(function () {
-                                                                                connection.release();
-                                                                                return reject(err);
-                                                                            });
-                                                                        })
-                                                                    }
-                                                                }
-                                                            );
-                                                        }
-                                                    }
-                                                );
-                                            }
-                                        })
-                                }
-                            })
-                    }
-                }
-            })
-        } catch (error) {
-            return reject(error);
-        }
-    })
-}
+									connection.query(
+										'select * from roles where name=?',
+										[ data.role ],
+										(err, roleResult) => {
+											if (err) {
+												connection.rollback(function() {
+													connection.release();
+													return reject(err);
+												});
+											} else {
+												var role = roleResult[0];
+												connection.query(
+													'insert into userinrole(idusers,idroles) values(?,?)',
+													[ data.idusers, role.idroles ],
+													(err, result) => {
+														if (err) {
+															connection.rollback(function() {
+																connection.release();
+																return reject(err);
+															});
+														} else {
+															connection.query(
+																'insert into pejabat  (nama, nip, data, foto, idjabatan, idusers) values(?,?,?,?,?,?)',
+																[
+																	data.nama,
+																	data.nip,
+																	JSON.stringify(data.data),
+																	data.foto,
+																	data.idjabatan,
+																	data.idusers
+																],
+																(error, result) => {
+																	if (error) {
+																		return reject(error);
+																	} else {
+																		data.idpejabat = result.insertId;
+																		helper
+																			.sendEmailConfirmEmail(
+																				{
+																					idusers: data.idusers,
+																					email: data.data.email
+																				},
+																				hostname
+																			)
+																			.then(
+																				(x) => {
+																					connection.commit(function(err) {
+																						if (err) {
+																							return connection.rollback(
+																								function() {
+																									connection.release();
+																									return reject(err);
+																								}
+																							);
+																						} else {
+																							connection.release();
+																							return resolve(data);
+																						}
+																					});
+																				},
+																				(err) => {
+																					connection.rollback(function() {
+																						connection.release();
+																						return reject(err);
+																					});
+																				}
+																			);
+																	}
+																}
+															);
+														}
+													}
+												);
+											}
+										}
+									);
+								}
+							}
+						);
+					}
+				}
+			});
+		} catch (error) {
+			return reject(error);
+		}
+	});
+};
 
 pejabat.put = async (data) => {
-    return new Promise((resolve, reject) => {
-        try {
-            pool.query(
-                'update pejabat set nama=?, data=?, foto=?, idjabatan=?, idusers=? where idpejabat=? ',
-                [data.nama, JSON.stringify(data.data), data.foto, data.idjabatan, data.idusers, data.idpejabat],
-
-                (error, result) => {
-                    if (error) {
-                        return reject(error);
-                    } else
-
-                        resolve(data);
-                }
-            );
-        } catch (error) {
-            return reject(error);
-        }
-    });
+	return new Promise((resolve, reject) => {
+		try {
+			pool.query(
+				'update pejabat set nama=?, nip=?, data=?, foto=?, idjabatan=?, idusers=? where idpejabat=? ',
+				[
+					data.nama,
+					data.nip,
+					JSON.stringify(data.data),
+					data.foto,
+					data.idjabatan,
+					data.idusers,
+					data.idpejabat
+				],
+				(error, result) => {
+					if (error) {
+						return reject(error);
+					} else resolve(data);
+				}
+			);
+		} catch (error) {
+			return reject(error);
+		}
+	});
 };
 
 pejabat.delete = (id) => {
-    return new Promise((resolve, reject) => {
-        try {
-            pool.query('delete from pejabat where idpejabat=? ', [id], (error, result) => {
-                if (error) {
-                    return reject(error);
-                } else
-                    resolve(true);
-            });
-        } catch (error) {
-            return reject(error);
-        }
-    });
+	return new Promise((resolve, reject) => {
+		try {
+			pool.query('delete from pejabat where idpejabat=? ', [ id ], (error, result) => {
+				if (error) {
+					return reject(error);
+				} else resolve(true);
+			});
+		} catch (error) {
+			return reject(error);
+		}
+	});
 };
 
 module.exports = pejabat;
--- server/db/permohonan.db.js
+++ server/db/permohonan.db.js
@@ -2,40 +2,44 @@ const pool = require('./dbconnection');
 const db = {};
 
 db.get = async () => {
-    return new Promise((resolve, reject) => {
-        pool.query(
-            `SELECT
+	return new Promise((resolve, reject) => {
+		pool.query(
+			`SELECT
             permohonan.*,
             penduduk.nama,
             penduduk.nik,
             penduduk.nkk,
             jenispermohonan.nama AS namapermohonan,
-            jenispermohonan.jenis
+            jenispermohonan.jenis,
+            pejabat.nama AS namapejabat,
+            pejabat.nip,
+            jabatan.nama AS namajabatan
           FROM
             permohonan
             LEFT JOIN penduduk ON permohonan.idpenduduk = penduduk.idpenduduk
             LEFT JOIN jenispermohonan ON permohonan.idjenispermohonan =
-            jenispermohonan.idjenispermohonan`,
-            (err, result) => {
-                if (err) {
-                    return reject(err);
-                } else {
-                    result.forEach(element => {
-                        element.data = JSON.parse(element.data);
-                        element.persetujuan = JSON.parse(element.persetujuan)
-                    });
-                    resolve(result);
-                }
-            }
-        );
-    });
+          jenispermohonan.idjenispermohonan
+            LEFT JOIN pejabat ON permohonan.idpejabat = pejabat.idpejabat
+            LEFT JOIN jabatan ON pejabat.idpejabat = jabatan.idjabatan`,
+			(err, result) => {
+				if (err) {
+					return reject(err);
+				} else {
+					result.forEach((element) => {
+						element.data = JSON.parse(element.data);
+						element.persetujuan = JSON.parse(element.persetujuan);
+					});
+					resolve(result);
+				}
+			}
+		);
+	});
 };
 
-
 db.getMyPermohonan = async (id) => {
-    return new Promise((resolve, reject) => {
-        pool.query(
-            `SELECT
+	return new Promise((resolve, reject) => {
+		pool.query(
+			`SELECT
             permohonan.*,
             penduduk.nama,
             penduduk.nik,
@@ -46,26 +50,27 @@ db.getMyPermohonan = async (id) => {
             permohonan
             LEFT JOIN penduduk ON permohonan.idpenduduk = penduduk.idpenduduk
             LEFT JOIN jenispermohonan ON permohonan.idjenispermohonan =
-          jenispermohonan.idjenispermohonan where penduduk.idusers=?`, [id],
-            (err, result) => {
-                if (err) {
-                    return reject(err);
-                } else {
-                    result.forEach(element => {
-                        element.data = JSON.parse(element.data);
-                        element.persetujuan = JSON.parse(element.persetujuan)
-                    });
-                    resolve(result);
-                }
-            }
-        );
-    });
+          jenispermohonan.idjenispermohonan where penduduk.idusers=?`,
+			[ id ],
+			(err, result) => {
+				if (err) {
+					return reject(err);
+				} else {
+					result.forEach((element) => {
+						element.data = JSON.parse(element.data);
+						element.persetujuan = JSON.parse(element.persetujuan);
+					});
+					resolve(result);
+				}
+			}
+		);
+	});
 };
 
 db.getById = async (Id) => {
-    return new Promise((resolve, reject) => {
-        pool.query(
-            `SELECT
+	return new Promise((resolve, reject) => {
+		pool.query(
+			`SELECT
             permohonan.*,
             penduduk.nama,
             penduduk.nik,
@@ -77,30 +82,28 @@ db.getById = async (Id) => {
             LEFT JOIN penduduk ON permohonan.idpenduduk = penduduk.idpenduduk
             LEFT JOIN jenispermohonan ON permohonan.idjenispermohonan =
           jenispermohonan.idjenispermohonan where idpermohonan=? `,
-            [Id],
-            (err, result) => {
-                if (err) {
-                    return reject(err);
-                } else {
-                    if (result.length <= 0)
-                        resolve(result)
-                    else {
-                        var item = result[0];
-                        item.data = JSON.parse(item.data);
-                        item.persetujuan = JSON.parse(item.persetujuan);
-                        resolve(item);
-                    }
-
-                }
-            }
-        );
-    });
+			[ Id ],
+			(err, result) => {
+				if (err) {
+					return reject(err);
+				} else {
+					if (result.length <= 0) resolve(result);
+					else {
+						var item = result[0];
+						item.data = JSON.parse(item.data);
+						item.persetujuan = JSON.parse(item.persetujuan);
+						resolve(item);
+					}
+				}
+			}
+		);
+	});
 };
 
 db.getByJenis = async (Id) => {
-    return new Promise((resolve, reject) => {
-        pool.query(
-            `SELECT
+	return new Promise((resolve, reject) => {
+		pool.query(
+			`SELECT
             permohonan.*,
             penduduk.nama,
             penduduk.nik,
@@ -112,83 +115,90 @@ db.getByJenis = async (Id) => {
             LEFT JOIN penduduk ON permohonan.idpenduduk = penduduk.idpenduduk
             LEFT JOIN jenispermohonan ON permohonan.idjenispermohonan =
           jenispermohonan.idjenispermohonan where permohonan.idjenispermohonan=? `,
-            [Id],
-            (err, result) => {
-                if (err) {
-                    return reject(err);
-                } else {
-                    result.forEach(element => {
-                        element.data = JSON.parse(element.data);
-                        element.persetujuan = JSON.parse(element.persetujuan)
-                    });
-                    resolve(result);
-                }
-            }
-        );
-    });
+			[ Id ],
+			(err, result) => {
+				if (err) {
+					return reject(err);
+				} else {
+					result.forEach((element) => {
+						element.data = JSON.parse(element.data);
+						element.persetujuan = JSON.parse(element.persetujuan);
+					});
+					resolve(result);
+				}
+			}
+		);
+	});
 };
-
-
 
 db.post = async (data) => {
-    return new Promise((resolve, reject) => {
-        try {
-            data.tanggalpengajuan = new Date();
-            pool.query(
-                'insert into permohonan  (idpenduduk,tanggalpengajuan, data,persetujuan,idjenispermohonan, nomorsurat) values(?,?,?,?,?,?)',
-                [data.idpenduduk, data.tanggalpengajuan, JSON.stringify(data.data), JSON.stringify(data.persetujuan), data.idjenispermohonan, data.nomorsurat],
-                (err, result) => {
-                    if (err) {
-                        return reject(err);
-                    } else {
-                        data.idpermohonan = result.insertId;
-                        resolve(data);
-                    }
-                }
-            );
-        } catch (err) {
-            return reject(err);
-        }
-    });
+	return new Promise((resolve, reject) => {
+		try {
+			data.tanggalpengajuan = new Date();
+			pool.query(
+				'insert into permohonan  (idpenduduk,tanggalpengajuan, data,persetujuan,idjenispermohonan, nomorsurat) values(?,?,?,?,?,?)',
+				[
+					data.idpenduduk,
+					data.tanggalpengajuan,
+					JSON.stringify(data.data),
+					JSON.stringify(data.persetujuan),
+					data.idjenispermohonan,
+					data.nomorsurat
+				],
+				(err, result) => {
+					if (err) {
+						return reject(err);
+					} else {
+						data.idpermohonan = result.insertId;
+						resolve(data);
+					}
+				}
+			);
+		} catch (err) {
+			return reject(err);
+		}
+	});
 };
 
-
 db.put = async (data) => {
-    return new Promise((resolve, reject) => {
-        try {
-            data.tanggalpengajuan = new Date(data.tanggalpengajuan);
-            pool.query(
-                'update permohonan set idpenduduk=?,tanggalpengajuan=?, data=?, persetujuan=?, idjenispermohonan=?, nomorsurat=? where idpermohonan=? ',
-                [data.idpenduduk, data.tanggalpengajuan, JSON.stringify(data.data), JSON.stringify(data.persetujuan),
-                    data.idjenispermohonan, data.nomorsurat, data.idpermohonan
-                ],
-                (err, result) => {
-                    if (err) {
-                        return reject(err);
-                    } else
-
-                        resolve(data);
-                }
-            );
-        } catch (err) {
-            return reject(err);
-        }
-    });
+	return new Promise((resolve, reject) => {
+		try {
+			data.tanggalpengajuan = new Date(data.tanggalpengajuan);
+			pool.query(
+				'update permohonan set idpenduduk=?,tanggalpengajuan=?, data=?, persetujuan=?, idjenispermohonan=?, nomorsurat=? where idpermohonan=? ',
+				[
+					data.idpenduduk,
+					data.tanggalpengajuan,
+					JSON.stringify(data.data),
+					JSON.stringify(data.persetujuan),
+					data.idjenispermohonan,
+					data.nomorsurat,
+					data.idpermohonan
+				],
+				(err, result) => {
+					if (err) {
+						return reject(err);
+					} else resolve(data);
+				}
+			);
+		} catch (err) {
+			return reject(err);
+		}
+	});
 };
 
 db.delete = (id) => {
-    return new Promise((resolve, reject) => {
-        try {
-            pool.query('delete from permohonan where idpermohonan=? ', [id], (err, result) => {
-                if (err) {
-                    return reject(err);
-                } else
-                    resolve(true);
-            });
-        } catch (err) {
-            return reject(err);
-        }
-    });
+	return new Promise((resolve, reject) => {
+		try {
+			pool.query('delete from permohonan where idpermohonan=? ', [ id ], (err, result) => {
+				if (err) {
+					return reject(err);
+				} else resolve(true);
+			});
+		} catch (err) {
+			return reject(err);
+		}
+	});
 };
 
 module.exports = db;
